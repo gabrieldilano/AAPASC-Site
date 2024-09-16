49e997cd0d0b969abf93efc9ce6eb0ef7499ff5f
gabrieldilano <ilanogabriel144@gmail.com>
2024-09-16 16:42:17 -0700

    removed carousel section and created section for landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import HeroSection from "./HeroSection";
 import RolesSection from "./RolesSection";
 import { SmoothScrollHero } from "./SmoothScrollHero";
+import LandingSection from "./LandingSection";
 
 export default function Home() {
     return (
         <>
+        <LandingSection/>
         <SmoothScrollHero/>
         <HeroSection />
         <RolesSection />
--- src/components/SmoothScrollHero.js
+++ src/components/SmoothScrollHero.js
@@ -5,28 +5,10 @@ import {
   useTransform,
 } from "framer-motion";
 import { useRef } from "react";
-import splitStringUsingRegex from "./splitStringUsingRegex";
-
-const charVariants = {
-    hidden: { opacity: 0 },
-    reveal: { opacity: 1 }
-};
 
 export const SmoothScrollHero = () => {
   return (
     <div className="bg-white">
-        <div className="flex items-center justify-center">
-            <div className="max-w-7xl w-full p-4">
-                <motion.h1 initial="hidden" whileInView="reveal" transition= {{staggerChildren: 0.02}} className="optigurney text-9xl text-right mt-[40vh]">{splitStringUsingRegex("WELCOME").map(char => (
-                <motion.span key={char} transition={{ duration: 1}} variants={charVariants}>{char}</motion.span>
-                ))}
-                </motion.h1>
-                <motion.p initial="hidden" whileInView="reveal" transition= {{staggerChildren: 0.02}} className="michroma text-xl text-right mt-2">{splitStringUsingRegex("WE ARE THE ASIAN / ASIAN PACIFIC AMERICAN STUDENT COALITION OF ASU").map(char => (
-                <motion.span key={char} transition={{ duration:0.5}} variants={charVariants}>{char}</motion.span>
-                ))}
-                </motion.p>
-            </div>
-        </div>
         <Hero />
     </div>
   );
@@ -41,9 +23,7 @@ const Hero = () => {
       className="relative w-full"
     >
       <CenterImage />
-
       <ParallaxImages />
-
       <div className="absolute bottom-0 left-0 right-0 h-96" />
     </div>
   );
